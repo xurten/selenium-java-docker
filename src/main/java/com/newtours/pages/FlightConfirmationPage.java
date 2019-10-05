7b5eeaab16fa23c9d38dbb0103e8e90a56cd0c7d
xurten <xurten@gmail.com>
2019-10-05 20:08:31 +0200

    Added FlightConfirmationPage

---
+++ src/main/java/com/newtours/pages/FlightConfirmationPage.java
@@ -0,0 +1,35 @@
+package com.newtours.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class FlightConfirmationPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(xpath = "//font[contains(text(), 'Flight Confirmation')]")
+    private WebElement flightConfirmationHeader;
+
+    @FindBy(xpath = "//font[contains(text(), 'USD')]")
+    private List<WebElement> prices;
+
+    public FlightConfirmationPage()
+    {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 30);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void printConfirmation()
+    {
+        this.wait.until(ExpectedConditions.visibilityOf(this.flightConfirmationHeader));
+        System.out.println(this.flightConfirmationHeader.getText());
+        System.out.println(this.prices.get(1).getText());
+    }
+}
