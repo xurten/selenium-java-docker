4808b8c6a9a1081c9e0f7d9b676821e172a218dd
Andrzej <xurten@gmail.com>
2019-10-25 22:14:55 +0200

    Added joda time

--- src/main/java/com/utils/DateUtils.java
+++ src/main/java/com/utils/DateUtils.java
@@ -1,13 +1,14 @@
 package com.utils;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class DateUtils {
     public static String GetActualDate()
     {
-        LocalDateTime dateTime = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return dateTime.format(formatter);
+        LocalDateTime currentDateAndTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        return currentDateAndTime.toString(formatter);
     }
 }
